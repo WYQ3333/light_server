2d81d827b8fbdd4f85add735bac9b51a058714df
WYQ3333 <3538922590@qq.com>
2019-03-30 21:39:43 +0800

    result

--- everday/3_30_red_money.cpp
+++ everday/3_30_red_money.cpp
@@ -4,27 +4,63 @@
 using namespace std;
 class Gift {
 public:
-    int getValue(vector<int> gifts, int n) {
-        // write code here
-        sort(gifts.begin(), gifts.end());
-        int result = gifts[0];
-        int count = 0;
-        int index = 0;
-        for(int i = 0; i < n; ++i){
-            if(gifts[i] == result){
-                count++;
-            }
-            else{
-                if(count >= (n/2+1)){
-                    index = i-1;
-                }
-                count = 1;
-                result = gifts[i];
-            }
-        }
-        return gifts[index];
-    }
+ int getValue(vector<int> gifts, int n) {
+     // write code here
+     sort(gifts.begin(), gifts.end());
+     if(n == 1){
+         return gifts[0];
+     }
+     if(n == 2 && gifts[0]==gifts[1]){
+         return gifts[0];
+     }
+     if(n == 2 && gifts[0]!=gifts[1]){
+         return 0;
+     }
+     int result = gifts[0];
+     int count = 0;
+     int index = 0;
+     for(int i = 0; i < n; ++i){
+         if(gifts[i] == result){
+             count++;
+         }
+         else{
+             if(count >= (n/2+1)){
+                 index = i-1;
+             }
+             count = 1;
+             result = gifts[i];
+         }
+     }
+     int temp = 0;
+     if(index != 0){
+         temp = gifts[index];
+     }
+     return temp;
+ }
 };
+/* class Gift { */
+/* public: */
+/*     int getValue(vector<int> gifts, int n) { */
+/*         // write code here */
+/*         sort(gifts.begin(), gifts.end()); */
+/*         int result = gifts[0]; */
+/*         int count = 0; */
+/*         int index = 0; */
+/*         for(int i = 0; i < n; ++i){ */
+/*             if(gifts[i] == result){ */
+/*                 count++; */
+/*             } */
+/*             else{ */
+/*                 if(count >= (n/2+1)){ */
+/*                     index = i-1; */
+/*                 } */
+/*                 count = 1; */
+/*                 result = gifts[i]; */
+/*             } */
+/*         } */
+/*         return gifts[index]; */
+/*     } */
+/* }; */
 int main()
 {
     Gift g;
