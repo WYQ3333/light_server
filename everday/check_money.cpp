30f7a799ac3b2fc9bc9d7e78e1159a72d5f1d01d
WYQ3333 <3538922590@qq.com>
2019-04-06 12:23:23 +0800

    perfect

--- everday/check_money.cpp
+++ everday/check_money.cpp
@@ -16,8 +16,8 @@ using namespace std;
 /* } */
 
 long long solution(long long input){
-    if(input <= 3){
-        return 1;
+    if(input == 0){
+        return 0;
     }
     long long count = 0;
     while(input >= 2){
