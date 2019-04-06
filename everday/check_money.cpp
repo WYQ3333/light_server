01837989b4e35d108ff9412ef434890ea3b2412f
WYQ3333 <3538922590@qq.com>
2019-04-06 12:20:08 +0800

    perfect

--- everday/check_money.cpp
+++ everday/check_money.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int solution(int input){
+/* int solution(int input){ */
+/*     if(input <= 3){ */
+/*         return 1; */
+/*     } */
+/*     int result = 0; */
+/*     if(input%2 == 0){ */
+/*         result = 1 + solution(input/2); */
+/*     } */
+/*     if(input%2 != 0){ */
+/*         result = 1 + 2*solution(input/2); */
+/*     } */
+/*     return result; */
+/* } */
+
+long long solution(long long input){
     if(input <= 3){
         return 1;
     }
-    return 1 + solution(input/2);
+    long long count = 0;
+    while(input >= 2){
+        if(input%3){
+            input = input/3 + 1;
+        }
+        else{
+            input /= 3;
+        }
+        count++;
+    }
+    return count;
 }
 int main()
 {
-    int input;
+    long long  input;
     while(cin >> input){
         if(input == 0){
             break;
