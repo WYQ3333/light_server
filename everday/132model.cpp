0aa5169a98da074bdd0ca79e227ff18a64e264f1
WYQ3333 <3538922590@qq.com>
2019-04-17 23:55:31 +0800

    解决不了数字重复的情况

--- everday/132model.cpp
+++ everday/132model.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<queue>
 #include<vector>
 using namespace std;
 
@@ -16,6 +17,52 @@ bool solution(vector<int>& array){
     return false;
 }
 
+int findqtop(vector<int>& nums, int index){
+    int result = -1; 
+    for(size_t i = 0; i < nums.size(); ++i){
+        if(nums[i] == index){
+            result = i;
+        }
+    }
+    return result;
+}
+
+bool find132(vector<int>& nums){
+    if(nums.size() < 3){
+        return false;
+    }
+    priority_queue<int, vector<int>> q;
+    for(size_t i = 0; i < nums.size(); ++i){
+        q.push(nums[i]);
+    }
+    while(!q.empty()){
+        int index = q.top();
+        int indexi = 0;
+        indexi = findqtop(nums, index);
+        if(indexi == (nums.size() - 1)){
+            return false;
+        }
+        int min_x = nums[0];
+        for(size_t i = 0; i < indexi; ++i){
+            if(nums[i] < min_x){
+                min_x = nums[i];
+            }
+        }
+        int max_x = nums[nums.size() -1];
+        for(size_t i = nums.size() - 1; i > indexi; --i){
+            if(nums[i] > max_x){
+                max_x = nums[i];
+            }
+        }
+
+        if(min_x < max_x && min_x < index && index > max_x){
+            return true;
+        }
+        q.pop();
+    }
+    return false;
+}
+
 int main()
 {
     vector<int> array;
@@ -26,7 +73,7 @@ int main()
         cin >> temp;
         array.push_back(temp);
     }
-    cout << solution(array) << endl;
+    cout << find132(array) << endl;
     return 0;
 }
 
