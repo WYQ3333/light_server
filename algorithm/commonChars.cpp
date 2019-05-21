139bba857c09d708907d8533b80c2bffe588c547
WYQ3333 <3538922590@qq.com>
2019-05-21 12:39:42 +0800

    python解法

--- algorithm/commonChars.cpp
+++ algorithm/commonChars.cpp
@@ -53,4 +53,20 @@ int main()
     cout << endl;
     return 0;
 }
+//python解法，只需取第一个字符串中的所有字符与其他字符进行比较即可
+class Solution:
+    def commonChars(self, A: List[str]) -> List[str]:
+        result = [];
+        # 队列为空，或者队列长度小于2，返回空
+        if not A or len(A) < 2:
+            return result;
 
+        # 获取队列中第一位的所有的字符
+        setKey = set(A[0])
+
+        # 遍历字符，并获取所有字符串中出现的次数的最小值
+        for x in setKey:
+            num = min(a.count(x) for a in A)
+            for i in range(num):
+                result.append(x)
+        return result
