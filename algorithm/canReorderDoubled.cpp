f6f3e1992ee4a31247b2ab12f14364c21d742e3e
WYQ3333 <3538922590@qq.com>
2019-07-08 16:50:46 +0800

    error

--- algorithm/canReorderDoubled.cpp
+++ algorithm/canReorderDoubled.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class Solution{
 public:
-    bool canReorderDoubled(vector<int>& array){
+    bool canReorderDoubled2(vector<int>& array){
         if(array.size() == 0){
             return true;
         }
@@ -33,6 +33,42 @@ public:
         }
         return true;
     }
+    bool find_2_num(vector<int>& array, int pos, int num, size_t& index){
+        for(size_t i = pos; i < array.size(); ++i){
+            if(array[i] == 2*num){
+                index = i;
+                return true;
+            }
+            else if(num == 2*array[i]){
+                index = i - 1;
+                return true;
+            }
+        }
+        return false;
+    }
+    bool canReorderDoubled(vector<int>& array){
+        if(array.size() == 0){
+            return true;
+        }
+        for(size_t i = 0; i < array.size(); ++i){
+            if(array[i] < 0){
+                array[i] = -array[i];
+            }
+        }
+        size_t index = 0;
+        size_t ret_index = 0;
+        while(index < array.size()){
+            if(find_2_num(array, index + 1, array[index], ret_index)){
+                if(ret_index != index + 1)
+                    swap(array[index + 1], array[ret_index]);
+                index += 2;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
 
     void Print(vector<int>& array){
         for(size_t i = 0; i < array.size(); ++i){
@@ -46,7 +82,7 @@ int main()
 {
     Solution s;
     //vector<int> array = {4,-2,2,-4};
-    vector<int> array = {-2,-6,-3,4,-4,2};
+    vector<int> array = {4,-4,4,-2,-4,-8};
     s.Print(array);
     cout << s.canReorderDoubled(array) << endl;
     s.Print(array);
